8055155fe21eca5c71cf3bc6048f5c3ae9fc7b58
samuelmemmi <samuelmemmi29@gmail.com>
2023-02-13 19:30:36 +0200

    process data for churn

--- data/process_data.py
+++ data/process_data.py
@@ -1,51 +1,50 @@
 import pandas as pd
 import numpy as np
 import torch
-import sklearn.cluster as cluster
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
-# Load the customer churn dataset into dataframe.
-churn_df = pd.read_csv('Customer_Churn.csv')
-# print(churn_df.head(5))
 
-# Total Charges column was categorical but must be a numerical value.
-# hence we convert it to categorical and fill with NAN values if there are errors.
-churn_df["TotalCharges"] = pd.to_numeric(churn_df["TotalCharges"], errors='coerce')
+def process_data_churn(file):
+    # Load the customer churn dataset into dataframe.
+    churn_df = pd.read_csv(file)
+    # print(churn_df.head(5))
 
-# Find out how many Nan values are in the dataset now
-# print(churn_df.isnull().sum())
-# drop NaN values since they are small
-churn_df.dropna(subset=["TotalCharges"], inplace=True)
-# print(churn_df.isnull().sum())
-# also we don't need the customer ID columm
-churn_df.drop("customerID", axis=1, inplace=True)
-# As expected there are no missing values in any of the columns. Dataset seems to be clean
+    # Total Charges column was categorical but must be a numerical value.
+    # hence we convert it to categorical and fill with NAN values if there are errors.
+    churn_df["TotalCharges"] = pd.to_numeric(churn_df["TotalCharges"], errors='coerce')
 
-# display general statistics of dataset
-# print(churn_df.describe())
+    # Find out how many Nan values are in the dataset now
+    # print(churn_df.isnull().sum())
+    # drop NaN values since they are small
+    churn_df.dropna(subset=["TotalCharges"], inplace=True)
+    # print(churn_df.isnull().sum())
+    # also we don't need the customer ID columm
+    churn_df.drop("customerID", axis=1, inplace=True)
+    # As expected there are no missing values in any of the columns. Dataset seems to be clean
 
-# maping of male to 1 and female to zero.
-churn_df['gender'] = churn_df['gender'].map({'Male': 1, 'Female': 0})
-# maping of yes to 1 and No to 0 for the following column
-churn_df['Partner'] = churn_df['Partner'].map({'Yes': 1, 'No': 0})
-churn_df['Dependents'] = churn_df['Dependents'].map({'Yes': 1, 'No': 0})
-churn_df['PhoneService'] = churn_df['PhoneService'].map({'Yes': 1, 'No': 0})
-churn_df['PaperlessBilling'] = churn_df['PaperlessBilling'].map({'Yes': 1, 'No': 0})
-churn_df['Churn'] = churn_df['Churn'].map({'Yes': 1, 'No': 0})
-# print(churn_df.head(5))
+    # display general statistics of dataset
+    # print(churn_df.describe())
 
-# get dummary for categorical columns now
-churn_df = pd.get_dummies(churn_df)
-churn_df = churn_df.astype(float)
-# print(churn_df.head(5))
+    # maping of male to 1 and female to zero.
+    churn_df['gender'] = churn_df['gender'].map({'Male': 1, 'Female': 0})
+    # maping of yes to 1 and No to 0 for the following column
+    churn_df['Partner'] = churn_df['Partner'].map({'Yes': 1, 'No': 0})
+    churn_df['Dependents'] = churn_df['Dependents'].map({'Yes': 1, 'No': 0})
+    churn_df['PhoneService'] = churn_df['PhoneService'].map({'Yes': 1, 'No': 0})
+    churn_df['PaperlessBilling'] = churn_df['PaperlessBilling'].map({'Yes': 1, 'No': 0})
+    churn_df['Churn'] = churn_df['Churn'].map({'Yes': 1, 'No': 0})
+    # print(churn_df.head(5))
 
-# Extract the features and labels
-X = churn_df.drop(['Churn'], axis=1)
-Y = churn_df['Churn']
-# we standardize the features
-scaler = StandardScaler()
-X_scaled = scaler.fit_transform(X)
+    # get dummary for categorical columns now
+    churn_df = pd.get_dummies(churn_df)
+    churn_df = churn_df.astype(float)
+    # print(churn_df.head(5))
 
-#  we split the data into training and testing sets
-X_train, X_test, Y_train, Y_test = train_test_split(X_scaled, Y, test_size=0.2, random_state=42)
+    # Extract the features and labels
+    X = churn_df.drop(['Churn'], axis=1)
+    Y = churn_df['Churn']
+
+    #  we split the data into training and testing sets
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    return churn_df, X_train, X_test, Y_train, Y_test
