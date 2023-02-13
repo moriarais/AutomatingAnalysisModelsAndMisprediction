c760bafa58e8b41e94b38fe64ffa666a5dca9dc2
Moris <moriarais@gmail.com>
2023-02-13 13:59:41 +0200

    feat: organize files to folder

--- main.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import torch
-import sklearn.cluster as cluster
-
-# Load the customer churn dataset into dataframe
-churn_df = pd.read_csv('Customer_Churn.csv')
-print(churn_df.head(5))
--- src/main.py
+++ src/main.py
@@ -1,34 +1,8 @@
-"""
-Create an iris flow
-"""
-from config import Location, ModelParams, ProcessConfig
-from prefect import flow
-from process import process
-from run_notebook import run_notebook
-from train_model import train
+import pandas as pd
+import numpy as np
+import torch
+import sklearn.cluster as cluster
 
-
-@flow
-def iris_flow(
-    location: Location = Location(),
-    process_config: ProcessConfig = ProcessConfig(),
-    model_params: ModelParams = ModelParams(),
-):
-    """Flow to run the process, train, and run_notebook flows
-
-    Parameters
-    ----------
-    location : Location, optional
-        Locations of inputs and outputs, by default Location()
-    process_config : ProcessConfig, optional
-        Configurations for processing data, by default ProcessConfig()
-    model_params : ModelParams, optional
-        Configurations for training models, by default ModelParams()
-    """
-    process(location, process_config)
-    train(location, model_params)
-    run_notebook(location)
-
-
-if __name__ == "__main__":
-    iris_flow()
+# Load the customer churn dataset into dataframe
+churn_df = pd.read_csv('Customer_Churn.csv')
+print(churn_df.head(5))
