fb5b29f9777e0b916fd3659db17cfde585afa256
Moris <moriarais@gmail.com>
2023-02-20 12:07:35 +0200

    Add return statement

--- src/process/process_creditcard.py
+++ src/process/process_creditcard.py
@@ -3,6 +3,7 @@
 import joblib
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import pickle
 
 import config.config_creditcard as config_creditcard
 
@@ -53,6 +54,14 @@ def save_processed_data(data: dict, save_location: str):
     """
     joblib.dump(data, save_location)
 
+def print_pkl_file(file_path: str):
+    # read python dict back from the file
+    pkl_file = open(file_path, 'rb')
+    mydict2 = pickle.load(pkl_file)
+    pkl_file.close()
+    print(mydict2)
+
+
 def process(
     location: config_creditcard.Location = config_creditcard.Location
 ):
@@ -65,9 +74,9 @@ def process(
     # 1. Null values handler. There are no null values in DB.
     # print(data.isnull().sum())
     # data.dropna(inplace=True)
-    #print(data.isnull().sum())
+    # print(data.isnull().sum())
     
-    # 2. All the fields are required for the ml process
+    # 2. All the features are required for the ml process
     
     # 3. Convert to numeric Class feature
     # 0 - non-fraudulent. 1 - fraudulent.
@@ -75,10 +84,13 @@ def process(
 
     processed = data
     # After cleaning and processing the database, display general statistics of dataset
-    print(processed.describe())    
+    # print(processed.describe())
 
     X, y = get_X_y(processed, config_creditcard.ProcessConfig.label)
     split_data = split_train_test(X, y, config_creditcard.ProcessConfig.test_size)
     save_processed_data(split_data, config_creditcard.Location.data_process)
+    return split_data
+    # print_pkl_file(config_creditcard.Location.data_process)
 
-process()
+if __name__ == "__main__":
+    process()
